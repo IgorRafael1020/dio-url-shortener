9d2746db8cc2cf264dbbd4bc45840b9b1c7cff3d
Igor <igorrafael_9@hotmail.com>
2022-03-23 18:17:07 -0300

    Upload Project

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://igorrafael1020:<password>@url-shortener-dio.tcxu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
